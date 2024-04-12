50e69d7007f5e703bd5d7330240ccacc57ee0161
Edras <monterrosoedras@gmail.com>
2024-04-11 23:13:40 -0600

    se cambio la resolucion de la pantalla

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import cv2
 
-
 def coleccionar_datos():
-    captura = cv2.VideoCapture(1)
-    captura.set(3,180)
+    #aqui es donde se indica que camara se va a usar 0 es la camara de la pc, 1 puede ser una webCam, y asi sucesivamente
+    #depende cuantas camaras le tengan a su pc
+    captura = cv2.VideoCapture(0)
+    captura.set(3, 1280)
     captura.set(4, 720)
 
     contador = 0
@@ -12,10 +13,11 @@ def coleccionar_datos():
         _, frame = captura.read()
         tecla = cv2.waitKey(5)
 
-        #con la tecla de espacio se toman los fotogramas
+        #con la tecla de espacio se toman las fotos
         if tecla == 32:
             #aqui es donde se guardan las imagenes en el disco, crear carpeta images,
             #luego renombrar donde dice apartado por el nombre del objeto que les tocó
+            #para que se guarden las imagenes con el nombre del objeto
             cv2.imwrite(f"images/apartado_{contador}.jpg", frame)
 
             contador = contador + 1
